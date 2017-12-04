ae49c4b704ff918f4402b38e0e504c312cdce4e8
chenyu <luchenyuxx@gmail.com>
2017-12-04 18:19:03 +0100

    add some parse work

--- robo.py
+++ robo.py
@@ -1,6 +1,7 @@
 import subprocess
+import re
+import datetime
 from lxml import etree
-from parse import findall
 
 print('Welcome to temporobo!')
 
@@ -13,33 +14,50 @@ tree = etree.fromstring(result.stdout.decode('utf-8'))
 
 entries = tree.findall('./entry', tree.nsmap)
 
-class Activity:
-    """User's JIRA activity"""
-    def __init__(self, title, issueId, author, timestamp):
-        self.title = title
-        self.issueId = issueId
-        self.author = author
-        self.timestamp = timestamp
+def extractTitle(rawTitle):
+    isRead = True
+    result = ''
+    for c in rawTitle:
+        if(c == '<'):
+            isRead = False
+        elif(c == '>'):
+            isRead = True
+        elif(isRead):
+            result += c
+    return result
 
-    def __init__(self, e):
-        self.title = self.getTextOrElse(e.find('./title',e.nsmap), "can't get title")
-        #self.title = ''.join(r.fixed[0] for r in findall(">{}<", rawTitle))
-        self.issueId = self.getTextOrElse(e.find('./activity:object/title', e.nsmap), "can't get issue id")
-        self.author = self.getTextOrElse(e.find('./author/name', e.nsmap), "can't get author")
-        self.timestamp = self.getTextOrElse(e.find('./published', e.nsmap), "can't get timestamp")
-
-    def getTextOrElse(self, e, default):
+def getTextOrElse(e, default):
         if(e is not None):
             return e.text
         else:
             return default
 
+timefmt = '%Y-%m-%dT%H:%M:%S.%fZ'
+pattern = re.compile('((NDF|NDR)-\d+)')
+def findIdInTitle(title):
+    result = pattern.search(title)
+    if result is not None:
+        return result.group(1)
+    else:
+        return None
+
+class Activity:
+    """User's JIRA activity"""
+    def __init__(self, e):
+        rawTitle = extractTitle(getTextOrElse(e.find('./title',e.nsmap), "can't get title"))
+        self.title = ' '.join(rawTitle.split())
+        # self.issueId = getTextOrElse(e.find('./activity:object/title', e.nsmap), findIdInTitle(self.title))
+        self.issueId = findIdInTitle(self.title)
+        self.author = getTextOrElse(e.find('./author/name', e.nsmap), "can't get author")
+        timestring = getTextOrElse(e.find('./published', e.nsmap), "can't get timestamp")
+        self.date = datetime.datetime.strptime(timestring, timefmt)
+
+
 activities = list(map(lambda e: Activity(e), entries))
 
 for a in activities:
     print("==================================")
-    print(a.title)
-    print(''.join(r.fixed[0] for r in findall(">{}<", a.title)))
-    print(a.issueId)
-    print(a.author)
-    print(a.timestamp)
+    print('title: ' + a.title)
+    print('issue id: ' + (a.issueId or ""))
+    print('author: ' + a.author)
+    print('date: ' + str(a.date))
