f76b2139b4d8b7ebd9590f088cfe01caa352d902
chenyu <luchenyuxx@gmail.com>
2017-12-12 10:51:15 +0100

    code refactoring

---
+++ helpers.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import datetime
+from lxml import etree
+import re
+
+def _removeTags(string):
+    """ remove html tags from the input string """
+    chars = []
+    isRead = True
+    for c in string:
+        if(c == '<'):
+            isRead = False
+        elif c=='>':
+            isRead = True
+        elif isRead:
+            chars.append(c)
+    return ''.join(chars)
+
+JIRA_ID_PATTERN = re.compile('([A-Z]+-\d+)')
+def _findIssueId(string):
+    """ find JIRA issue id in a string """
+    result = JIRA_ID_PATTERN.search(string)
+    if result is not None:
+        return result.group(1)
+    else:
+        return None
+
+def _textOrElse(tree, default):
+    " get tree text, if tree is None, return default "
+    if tree is not None:
+        return tree.text
+    else:
+        return default
+
+NEOXAM_STREAM_URL = 'https://support.neoxam.com/activity?streams=user+IS+{user}+&streams=update-date+BETWEEN+{startTime}+{endTime}'
+def _getActivityStream(startMilli, endMilli, user, password):
+    """ get activity stream through http request """
+    url = NEOXAM_STREAM_URL.format(user=user, startTime=startMilli, endTime=endMilli)
+    result = requests.get(url, auth=(user, password))
+    return result.text
+
+def getActivities(startTime, endTime, user, password):
+    "request activity stream and parse it into a list of Activity objects"
+    startMilli = int(startTime.timestamp() * 1000)
+    endMilli = int(endTime.timestamp() * 1000)
+    rawStream = _getActivityStream(startMilli, endMilli, user, password)
+    tree = etree.fromstring(rawStream)
+    entries = tree.findall('./entry', tree.nsmap)
+    activities = list(map(lambda e: Activity(e), entries))
+    return activities
+
+NEOXAM_LOG_WORK_URL = 'https://support.neoxam.com/rest/api/2/issue/{issueId}/worklog'
+def logWork(issueId, startTime, secondSpent, user, password):
+    "send post request to server to log user's work, return the server response"
+    timeString = startTime.strftime('%Y-%m-%d') + 'T15:00:00.000+0000'
+    url = NEOXAM_LOG_WORK_URL.format(issueId=issueId)
+    header = {'Content-Type':'application/json'}
+    payload = json.dumps({'comment':'work', 'timeSpentSeconds':secondSpent, 'started':timeString})
+    response = requests.post(url, data=payload, headers=header, auth=(user, password))
+    return response
+
+class Activity:
+    "User's JIRA activity"
+    def __init__(self, tree):
+        "initial from a xml tree object"
+        rawTitle = _removeTags(_textOrElse(tree.find('./title', tree.nsmap), "can't get title"))
+        self.title = ' '.join(rawTitle.split())
+        self.issueId = _findIssueId(self.title)
+        self.author = _textOrElse(tree.find('./author/name', tree.nsmap), "can't get author")
+        timestring = _textOrElse(tree.find('./published', tree.nsmap), "can't get timestamp")
+        timefmt = '%Y-%m-%dT%H:%M:%S.%fZ'
+        self.date = datetime.datetime.strptime(timestring, timefmt)
--- robo.py
+++ robo.py
@@ -1,12 +1,11 @@
 import requests
 import schedule
-import re
 import datetime
-from lxml import etree
 import collections
 import time
 import argparse
 import json
+from helpers import (Activity, getActivities, logWork)
 
 def workon(aNiceDay, user, pwd):
     print('Going to log work for ' + str(aNiceDay))
@@ -15,60 +14,12 @@ def workon(aNiceDay, user, pwd):
     endOfDay = aNiceDay + 'T23:59:59'
     startOfDay = datetime.datetime.strptime(startOfDay, dayfmt)
     endOfDay = datetime.datetime.strptime(endOfDay, dayfmt)
-    startOfDay = int(startOfDay.timestamp() * 1000)
-    endOfDay = int(endOfDay.timestamp() * 1000)
 
-# Get the stream activities
-    streamUrl = 'https://support.neoxam.com/activity?streams=user+IS+'+user+'&streams=update-date+BETWEEN+' + str(startOfDay) + '+' + str(endOfDay)
-    result = requests.get(streamUrl, auth=(user, pwd))
+    activities = getActivities(startOfDay, endOfDay, user, pwd)
 
-    tree = etree.fromstring(result.text)
-
-    entries = tree.findall('./entry', tree.nsmap)
-    if len(entries) == 0:
+    if len(activities) == 0:
         print('You have no activities on ' + aNiceDay + ', neoxam family is not happy!')
         return
-
-    def extractTitle(rawTitle):
-        isRead = True
-        result = ''
-        for c in rawTitle:
-            if(c == '<'):
-                isRead = False
-            elif(c == '>'):
-                isRead = True
-            elif(isRead):
-                result += c
-        return result
-
-    def getTextOrElse(e, default):
-            if(e is not None):
-                return e.text
-            else:
-                return default
-
-    timefmt = '%Y-%m-%dT%H:%M:%S.%fZ'
-    idPattern = re.compile('((NDF|NDR)-\d+)')
-    def findIdInTitle(title):
-        result = idPattern.search(title)
-        if result is not None:
-            return result.group(1)
-        else:
-            return None
-
-    class Activity:
-        """User's JIRA activity"""
-        def __init__(self, e):
-            rawTitle = extractTitle(getTextOrElse(e.find('./title',e.nsmap), "can't get title"))
-            self.title = ' '.join(rawTitle.split())
-            # self.issueId = getTextOrElse(e.find('./activity:object/title', e.nsmap), findIdInTitle(self.title))
-            self.issueId = findIdInTitle(self.title)
-            self.author = getTextOrElse(e.find('./author/name', e.nsmap), "can't get author")
-            timestring = getTextOrElse(e.find('./published', e.nsmap), "can't get timestamp")
-            self.date = datetime.datetime.strptime(timestring, timefmt)
-
-
-    activities = list(map(lambda e: Activity(e), entries))
 
     for a in activities:
         print("==================================")
@@ -91,11 +42,7 @@ def workon(aNiceDay, user, pwd):
         frequency = issueFrequency[issue]
         print(str(frequency)+' activities on issue '+issue)
         secondSpent = int(totalSeconds * frequency / sumFrequency)
-        header = {'Content-Type':'application/json'}
-        started = aNiceDay+'T15:00:00.000+0000'
-        payload = json.dumps({'comment':'work', 'timeSpentSeconds':secondSpent, 'started':started})
-        url = 'https://support.neoxam.com/rest/api/2/issue/'+issue+'/worklog'
-        r = requests.post(url, data=payload, headers=header, auth=(user, pwd))
+        r = logWork(issue, startOfDay, secondSpent, user, pwd)
         if r.status_code == 201:
             print('Successfully log '+str(secondSpent)+' seconds work to issue '+issue)
         else:
@@ -127,7 +74,7 @@ if __name__ == '__main__':
     today = args.date
 
     if today is not None:
-        workon(today, usr, pwd)
+        workon(today, user, pwd)
     else:
         print('The robot will log your work on JIRA at 19:00 every day.')
         schedule.every().day.at('19:00').do(workNow(user, pwd))
